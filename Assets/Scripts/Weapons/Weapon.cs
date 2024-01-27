a4638d9d2955f681a8c19d150b82d4d1d90aca31
EOppedal <erlend.oppedal@gmail.com>
2024-01-27 12:32:47 +0100

    ThrowingTest

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Weapons
+{
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapon/New Weapon", order = 1)]
+    public class Weapon : ScriptableObject
+    {
+        public GameObject prefab;
+        public float weight;
+        
+        [Header("Throwable")]
+        public bool isThrowable;
+        public float throwingSpeed;
+        public float throwingDamage;
+        
+        [Header("Melee")]
+        public bool isMelee;
+        public float meleeDamage;
+    }
+}
